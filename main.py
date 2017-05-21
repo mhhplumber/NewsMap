bbc16af016e880190b544a4ea741d9042bf3088d
Beebkips <conorm413@gmail.com>
2017-05-20 23:26:01 -0700

    The coolest main.py

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-import json, requests
+import json, requests, nltk
+from textblob import TextBlob
+
+"""
+'breitbart-news',
+'buzzfeed',
+'entertainment-weekly',
+'espn',
+'espn-cric-info',
+'fox-sports',
+'ign',
+'talksport',
+'the-sport-bible',
+'football-italia',
+'bbc-sport',
+'nfl-news',
+'polygon',
+'wirtschafts-woche'
+"""
 
 sources= ['abc-news-au',
           'al-jazeera-english',
           'ars-technica',
           'associated-press',
           'bbc-news',
-          'bbc-sport',
           'bild',
           'bloomberg',
-          'breitbart-news',
           'business-insider',
           'business-insider-uk',
-          'buzzfeed',
           'cnbc',
           'cnn',
           'daily-mail',
           'der-tagesspiegel',
           'die-zeit',
           'engadget',
-          'entertainment-weekly',
-          'espn',
-          'espn-cric-info',
           'financial-times',
           'focus',
-          'football-italia',
           'fortune',
           'four-four-two',
-          'fox-sports',
           'google-news',
           'gruenderszene',
           'hacker-news',
           'handelsblatt',
-          'ign',
           'independent',
           'mashable',
           'metro',
@@ -42,14 +51,11 @@ sources= ['abc-news-au',
           'new-scientist',
           'newsweek',
           'new-york-magazine',
-          'nfl-news',
-          'polygon',
           'recode',
           'reddit-r-all',
           'reuters',
           'spiegel-online',
           't3n',
-          'talksport',
           'techcrunch',
           'techradar',
           'the-economist',
@@ -60,7 +66,6 @@ sources= ['abc-news-au',
           'the-lad-bible',
           'the-new-york-times',
           'the-next-web',
-          'the-sport-bible',
           'the-telegraph',
           'the-times-of-india',
           'the-verge',
@@ -68,11 +73,47 @@ sources= ['abc-news-au',
           'the-washington-post',
           'time',
           'usa-today',
-          'wired-de',
-          'wirtschafts-woche' ]
+          'wired-de', ]
 
-for api in sources:
-    # print('https://newsapi.org/v1/articles?source=' + api + '&sortBy=latest&apiKey=b506a06468994fcc9ed9f55451000921')
-    call = requests.get('https://newsapi.org/v1/articles?source=' + api + '&sortBy=latest&apiKey=b506a06468994fcc9ed9f55451000921')
-    payload = json.loads(call.text)
-    print(payload)
+# for api in sources:
+#     # print('https://newsapi.org/v1/articles?source=' + api + '&sortBy=latest&apiKey=b506a06468994fcc9ed9f55451000921')
+#     call = requests.get('https://newsapi.org/v1/articles?source=' + api + '&sortBy=top&apiKey=b506a06468994fcc9ed9f55451000921')
+#     payload = json.loads(call.text)
+#     # print(payload)
+#     if payload['status'] != 'error':
+#         print(payload['source'])
+#         for a in payload['articles']:
+#             print('\t' + a['title'])
+
+# Imports the Google Cloud client library
+from google.cloud import language
+
+# Instantiates a client
+language_client = language.Client()
+
+# The text to analyze
+
+call = requests.get('https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=b506a06468994fcc9ed9f55451000921')
+payload = json.loads(call.text)
+    # print(payload)
+if payload['status'] != 'error':
+    print(payload['source'])
+    for a in payload['articles']:
+        # print('\t' + a['title'])
+
+        text = a['title'] #'Jet collides with truck at LAX'
+        document = language_client.document_from_text(text)
+
+        # Detects the sentiment of the text
+        sentiment = document.analyze_sentiment().sentiment
+
+        # print(document.analyze_sentiment())
+        print('Text: {}'.format(text))
+        # print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
+
+        entities = document.analyze_entities().entities
+
+        for a in entities:
+            print('\t', a.name, a.entity_type)
+# print(entities[0].__dict__)
